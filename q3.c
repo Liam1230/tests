8e481805980823ca1bab87015751f7cc36665e55
Liam1230 <kinoko.sd@gmail.com>
2022-05-24 10:12:27 +0900

    標準入力を受け取るように対応

--- q3.c
+++ q3.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-char* caesarCipher(char* s,int c){
+char* caesarCipher(char* s,int n){
 
   int i = 0;
   int z = ('Z' - 'A') + 1;
 
   while(s[i] != '\0'){
-    s[i]= ((s[i] - 'A') + c + z) % z + 'A';
+    s[i]= ((s[i] - 'A') + n + z) % z + 'A';
     i++;
   }
 
@@ -16,8 +16,12 @@ char* caesarCipher(char* s,int c){
 
 int main(){
 
-  char str[] = "TRACK";
-  printf("%s\n", caesarCipher(str, 2));
+  char s[100];
+  int n;
+
+  scanf("%s %d", s, &n);
+
+  printf("%s\n", caesarCipher(s, n));
 
   return 0;
 }
